7c7ac53e67a03204cda6ca2d4c5a6c45b38dbbbf
Johnson Joseph <jhonsonj88@gmail.com>
2020-02-04 06:53:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+
 
 ReactDOM.render(
   <Home />,
